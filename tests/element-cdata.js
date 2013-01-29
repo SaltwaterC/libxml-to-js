0732adf18d8ea243da7b00566203640700e97b62
Ștefan Rusu <saltwaterc@gmail.com>
2013-01-29 17:58:50 +0200

    Implemented XApp-Studio's patch. Implemented a couple of tests for the CDATA issue. Closes #13.

--- tests/element-cdata.js
+++ tests/element-cdata.js
@@ -1,2 +1,20 @@
-console.error('placeholder for the element-cdata test');
-process.exit(1);
+var parser = require('../');
+
+var fs = require('fs');
+var assert = require('assert');
+
+var xml = fs.readFileSync('data/element-cdata.xml').toString();
+
+var callback = false;
+
+parser(xml, function (err, res) {
+	callback = true;
+	assert.ifError(err);
+	assert.strictEqual(res.phrase['#'], 'Home');
+	assert.strictEqual(res.phrase['@'].section, 'default');
+	assert.strictEqual(res.phrase['@'].code, 'widgethome');
+});
+
+process.on('exit', function () {
+	assert.ok(callback);
+});
--- tests/root-cdata.js
+++ tests/root-cdata.js
@@ -1,2 +1,21 @@
-console.error('placeholder for the root-cdata test');
-process.exit(1);
+var parser = require('../');
+
+var fs = require('fs');
+var assert = require('assert');
+
+var xml = fs.readFileSync('data/root-cdata.xml').toString();
+
+var callback = false;
+
+parser(xml, function (err, res) {
+	callback = true;
+	assert.ifError(err);
+	
+	assert.strictEqual(res['#'], 'Home');
+	assert.strictEqual(res['@'].section, 'default');
+	assert.strictEqual(res['@'].code, 'widgethome');
+});
+
+process.on('exit', function () {
+	assert.ok(callback);
+});
